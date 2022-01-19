cabbbb2dcd73519bf1d44d36dc8f6d5a5948f81f
arvinlauron <arvinglauron@gmail.com>
2022-01-19 18:40:42 +0800

    add avatar

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LikeButton from './LikeButton'
 import './App.css';
 import Avatar from '@mui/material/Avatar';
+import { deepOrange } from '@mui/material/colors';
 
 
 
@@ -13,7 +14,7 @@ function Post (props) {
     
         return (
                 <div className='post'>
-                    <Avatar className='avatar' >{name[0].toUpperCase()}</Avatar>
+                    <Avatar sx={{ bgcolor: deepOrange[500] }}>{name[0].toUpperCase()}</Avatar>
                     <h3>{name}</h3>
                     <p>{content}</p>
                     <p>Number of Likes</p>
